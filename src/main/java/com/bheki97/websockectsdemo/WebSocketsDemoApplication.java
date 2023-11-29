1ef794a5640ce6267d4854f0e1c73f160a635759
bheki97 <bfmokoena7@gmail.com>
2023-11-29 08:42:29 +0200

    Initial Commit

---
+++ src/main/java/com/bheki97/websockectsdemo/WebSocketsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bheki97.websockectsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bheki97/websockectsdemo/WebSocketsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bheki97.websockectsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
